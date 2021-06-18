5f6b2b07688421c2e714b4b2857c39eb973625bc
Pedro Henrique <pedrohenriquefrancadedeus@gmail.com>
2021-06-18 12:36:18 -0300

    fix: title game, button color, number button color, clear when add to cart, trash image, empty cart

--- app.js
+++ app.js
@@ -1,220 +1,275 @@
-const ajax = new XMLHttpRequest()
-const numbers = document.getElementById('numbers')
-const cardList = document.getElementById('cart-list')
-const totalPrice = document.getElementById('total-price')
-const gamesList = document.getElementById('game-types')
-const gameDescription = document.getElementById('description')
-const clearGameButton = document.getElementById('clearGameButton')
-const completeGameButton = document.getElementById('completeGameButton')
-const addToCartButton = document.getElementById('addToCartButton')
-const deleteCartButton = document.getElementById('deleteCartButton')
+;(function () {
+  'use strict'
 
-addToCartButton.addEventListener('click', handleAddToCart)
-clearGameButton.addEventListener('click', handleClearGame)
-completeGameButton.addEventListener('click', handleCompleteGame)
-// deleteCartButton.addEventListener('click', handleDeleteCart)
+  const $chooseGameButtons = document.querySelector(
+    '[data-js="choose-game-buttons"]'
+  )
+  const $betDescription = document.querySelector('[data-js="bet-description"]')
+  const $gameName = document.querySelector('[data-js="game-name"]')
+  const $numbersField = document.querySelector('[data-js="numbers"]')
+  const $bets = document.querySelector('[data-js="bets"]')
+  const $clearSelectedNumbers = document.querySelector(
+    '[data-js="clear-selected-numbers"]'
+  )
+  const $addToCartButton = document.querySelector(
+    '[data-js="add-to-cart-button"]'
+  )
+  const $completeGame = document.querySelector(
+    '[data-js="complete-game-numbers"]'
+  )
+  const $saveButton = document.querySelector('[data-js="save-button"]')
+  const $totalValueField = document.querySelector('[data-js="total-value"]')
 
-let data = []
-let selectedNumber = []
-let cart = []
-let currentGameMaxNumbers = 0
-let currentGameRange = 0
-let currentGamePrice = 0
-let currentGameType = ''
+  let selectedGame = {}
+  let games = []
+  let selectedNumbers = []
+  let bets = []
+  let betId = 0
+  let betTotalValue = 0
 
-function getData() {
-  ajax.open('GET', 'games.json')
-  ajax.send()
-  ajax.addEventListener('readystatechange', () => {
+  function init() {
+    getGamesJSON()
+  }
+
+  function getGamesJSON() {
+    const ajax = new XMLHttpRequest()
+    ajax.open('GET', './games.json')
+    ajax.send()
+    ajax.addEventListener('readystatechange', () => getAllGames(ajax))
+  }
+
+  function getAllGames(ajax) {
     if (ajax.readyState === 4 && ajax.status === 200) {
-      data = JSON.parse(ajax.responseText)
-      gamesList.innerHTML = [...data.types]
-        .map((game, index) => {
-          return index === 0
-            ? `
-                        <button
-                          id="${game.type}"
-                          active="true"
-                          class="gameButton ${game.type}"
-                          onclick="handlePickGame(${game['max-number']},${game.range},'${game.type}',${game.price},'${game.description}')">
-                          ${game.type}
-                          </button>`
-            : `
-                        <button
-                          id="${game.type}"
-                          class="gameButton ${game.type}"
-                          onclick="handlePickGame(${game['max-number']},${game.range},'${game.type}',${game.price},'${game.description}')">
-                          ${game.type}
-                        </button>
-                        `
-        })
-        .join('')
+      games = JSON.parse(ajax.responseText).types
+      createGameButtons()
+      listenFunctionalButtons()
     }
-  })
-}
-
-function formatCurrency(value) {
-  return (value = value.toLocaleString('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-  }))
-}
-
-number = document.querySelector('input#number')
-
-function handleSelectNumber(index, maxLimit) {
-  let newEntry = index + 1
-
-  if (selectedNumber.includes(newEntry)) {
-    selectedNumber.splice(selectedNumber.indexOf(newEntry), 1)
-    selectedNumber.sort((a, b) => a - b)
-    number[index].classList.remove('numberSelected')
-  } else if (selectedNumber.length >= maxLimit) {
-    alert(`Ja foram selecionado a quantidade maxima de números: ${maxLimit}`)
-  } else {
-    selectedNumber.push(newEntry)
-    selectedNumber.sort((a, b) => a - b)
-    number[index].classList.add('numberSelected')
-  }
-}
-
-function handleCompleteGame() {
-  const min = 1
-  let max = currentGameMaxNumbers
-  let range = currentGameRange
-  let currentArray = []
-
-  for (i = 1; i <= range; i++) {
-    currentArray.push(i)
   }
 
-  if (selectedNumber.length === 0) {
-    for (let i = 0; i < max; i++) {
-      let randomNum = Math.floor(Math.random() * range) + min
-      let check = selectedNumber.includes(randomNum)
+  function createGameButtons() {
+    games.forEach(game => {
+      $chooseGameButtons.insertAdjacentHTML(
+        'beforeend',
+        `<button class="game-button" data-js="${game.type}" >
+          ${game.type}
+        </button>`
+      )
+    })
+    addClickEventAndChangeButtonColor()
+    addFirstAccessData(changeSelectedButtonColor(0))
+  }
 
-      if (check === false) {
-        selectedNumber.push(randomNum)
-        number[randomNum - 1].classList.add('numberSelected')
+  function addFirstAccessData() {
+    fillGameData(getGameInfo(games[0].type)[0])
+  }
+
+  function getGameInfo(gameName) {
+    return games.filter(game => {
+      return game.type === gameName
+    })
+  }
+
+  function addClickEventAndChangeButtonColor() {
+    $chooseGameButtons.childNodes.forEach((button, index) => {
+      button.addEventListener('click', () => {
+        const gameName = getGameInfo(button.dataset.js)[0]
+        fillGameData(gameName)
+        changeSelectedButtonColor(index)
+      })
+    })
+  }
+
+  function changeSelectedButtonColor(index) {
+    $chooseGameButtons.childNodes.forEach((button, currentIndex) => {
+      const game = getGameInfo(button.dataset.js)[0]
+
+      if (currentIndex === index) {
+        button.setAttribute(
+          'style',
+          `background-color: ${game.color}; 
+          color: #FFFFFF; 
+          border: 2px solid ${game.color};`
+        )
       } else {
-        while (check === true) {
-          randomNum = Math.floor(Math.random() * range) + min
-          check = selectedNumber.includes(randomNum)
-          if (check === false) {
-            selectedNumber.push(randomNum)
-            number[randomNum - 1].classList.add('numberSelected')
+        button.setAttribute(
+          'style',
+          `background-color: #FFFFFF; 
+          color: ${game.color};  
+          border: 2px solid ${game.color};`
+        )
+      }
+    })
+  }
+
+  function fillGameData(gameName) {
+    $betDescription.innerText = gameName.description
+    $gameName.innerText = gameName.type.toUpperCase()
+
+    Object.assign(selectedGame, gameName)
+
+    clearNumbersField()
+    fillGameNumbers(gameName)
+
+    selectedNumbers = []
+    handleBetNumbers(gameName)
+  }
+
+  function fillGameNumbers(gameName) {
+    for (var index = 1; index <= gameName.range; index++) {
+      $numbersField.insertAdjacentHTML(
+        'beforeend',
+        `<button class="number" data-js="${index}" value="${index}">
+          ${index}
+        </button>`
+      )
+    }
+  }
+
+  function clearNumbersField() {
+    $numbersField.innerHTML = ''
+  }
+
+  function handleBetNumbers(gameName) {
+    $numbersField.childNodes.forEach(button => {
+      button.addEventListener('click', () => {
+        var index = selectedNumbers.indexOf(Number(button.value))
+        if (index >= 0) {
+          selectedNumbers.splice(index, 1)
+          button.setAttribute('style', 'background-color: #ADC0C4;')
+        } else {
+          if (selectedNumbers.length < gameName['max-number']) {
+            selectedNumbers.push(Number(button.value))
+            button.setAttribute('style', `background-color: ${gameName.color}`)
           }
         }
+      })
+    })
+  }
+
+  function listenFunctionalButtons() {
+    $clearSelectedNumbers.addEventListener('click', clearSelectedNumbers)
+    $addToCartButton.addEventListener('click', addToCart)
+    $completeGame.addEventListener('click', completeGame)
+    $saveButton.addEventListener('click', saveGame)
+  }
+
+  function clearSelectedNumbers() {
+    selectedNumbers.forEach(number => {
+      document
+        .querySelector(`[data-js="${number}"]`)
+        .setAttribute('style', 'background-color: #ADC0C4;')
+    })
+    selectedNumbers = []
+  }
+
+  function addToCart() {
+    if (selectedNumbers.length !== selectedGame['max-number']) {
+      return alert(
+        'Preencha todos os números para adicionar o jogo ao carrinho!'
+      )
+    }
+    createBet()
+    betId += 1
+    clearSelectedNumbers()
+  }
+
+  function createBet() {
+    $bets.insertAdjacentHTML(
+      'beforeend',
+      `<div class="bet-card" data-id="${betId}" data-js="bet${
+        selectedGame.type
+      }">
+        <img data-js="remove-bet-from-cart" src="assets/trash.svg"/>
+        <div class="bet${
+          selectedGame.type
+        } bet-interior" style="border-left: 4px solid ${
+        selectedGame.color
+      }; border-radius: 4px;">
+          <span class="bet-cart-numbers">${selectedNumbers
+            .sort((a, b) => a - b)
+            .join(', ')}</span>
+          <div class="bet-name-price">
+            <p class="bet-name" style="color: ${selectedGame.color}">${
+        selectedGame.type
+      }</p>
+            <span class="bet-price">${String(
+              selectedGame.price.toFixed(2)
+            ).replace('.', ',')}</span></div>
+        </div>
+      </div>`
+    )
+
+    bets.push({
+      id: betId,
+      type: selectedGame.type,
+      price: selectedGame.price,
+      numbers: selectedNumbers.sort((a, b) => a - b).join(', '),
+    })
+
+    console.log(bets)
+
+    betTotalValue += selectedGame.price
+
+    changeTotalValue()
+    removeBetFromCart()
+  }
+
+  function removeBetFromCart() {
+    const betToBeRemoved = document.querySelector(`[data-id="${betId}"]`)
+
+    betToBeRemoved.addEventListener('click', () => {
+      bets = bets.filter(bet => {
+        return bet.id !== Number(betToBeRemoved.dataset.id)
+      })
+
+      for (let index = 0; index < games.length; index++) {
+        if (betToBeRemoved.dataset.js === `bet${games[index].type}`) {
+          betTotalValue -= games[index].price
+        }
+        changeTotalValue()
+        betToBeRemoved.remove()
+      }
+    })
+  }
+
+  function changeTotalValue() {
+    $totalValueField.textContent = String(betTotalValue.toFixed(2)).replace(
+      '.',
+      ','
+    )
+  }
+
+  function completeGame() {
+    let randomNumber = 0
+    while (selectedNumbers.length < selectedGame['max-number']) {
+      randomNumber = Math.ceil(Math.random() * selectedGame.range)
+      if (!isInCurrentBet(randomNumber)) {
+        document.querySelector(`[data-js='${randomNumber}']`).click()
       }
     }
   }
 
-  if (selectedNumber.length > 0) {
-    let changedMax = max - selectedNumber.length
-    for (let i = 0; i < changedMax; i++) {
-      let randomNum = Math.floor(Math.random() * range) + min
-      let check = selectedNumber.includes(randomNum)
+  function isInCurrentBet(number) {
+    return selectedNumbers.some(item => {
+      return number === item
+    })
+  }
 
-      if (check === false) {
-        selectedNumber.push(randomNum)
-        number[randomNum - 1].classList.add('numberSelected')
-      } else {
-        while (check === true) {
-          randomNum = Math.floor(Math.random() * range) + min
-          check = selectedNumber.includes(randomNum)
-          if (check === false) {
-            selectedNumber.push(randomNum)
-            number[randomNum - 1].classList.add('numberSelected')
-          }
-        }
-      }
+  function saveGame() {
+    if (bets.length === 0) {
+      return alert('Seu carrinho está vazio!')
     }
-  }
-}
 
-function handleClearGame() {
-  selectedNumber.forEach(item => {
-    number[item - 1].classList.remove('numberSelected')
-  })
-  selectedNumber = []
-}
-
-function handleAddToCart() {
-  const listOfNumbers = selectedNumber.sort((a, b) => a - b).join()
-
-  let cardClass = ''
-  if (currentGameMaxNumbers === 15) {
-    cardClass = 'cartCardLotofacil'
-  }
-  if (currentGameMaxNumbers === 6) {
-    cardClass = 'cartCardMegasena'
-  }
-  if (currentGameMaxNumbers === 5) {
-    cardClass = 'cartCardQuina'
-  }
-
-  cardList.innerHTML += `
-                          <div class="cartCard" id="card-${currentGameMaxNumbers}">
-                              <img src="assets/trash.svg" class="cartCardIcon" id="deleteCartButton"/>
-                              <div class="${cardClass}Content">
-                                  <span class="cartCardNumbers">
-                                    ${listOfNumbers}
-                                  </span>
-                                  <div class="cartCardGame">
-                                    <strong class="${cardClass}">${currentGameType}</strong>
-                                    <p class="cartCardPrice">${formatCurrency(
-                                      currentGamePrice
-                                    )}</p>
-                                  </div>
-                              </div>
-                          </div>
-                          `
-  cart.push({
-    id: currentGameMaxNumbers,
-    price: currentGamePrice,
-  })
-  calculateTotal()
-}
-
-function handleDeleteCart(id) {
-  const item = document.getElementById(`card-${id}`)
-  item.parentNode.removeChild(item)
-  cart.map((item, index) => {
-    if (item.id === id) {
-      cart.splice(index, 1)
+    if (betTotalValue < games[0]['min-cart-value']) {
+      return alert('O valor mínimo de aposta é de R$ 30,00.')
     }
-  })
-  calculateTotal()
-}
 
-function calculateTotal() {
-  let cartTotal = 0
-  cart.map(item => {
-    cartTotal += item.price
-    return cartTotal
-  })
-  totalPrice.innerHTML = formatCurrency(cartTotal)
-}
-
-function handlePickGame(maxNumber, range, type, price, description) {
-  renderNumbers(maxNumber, range)
-  gameDescription.innerHTML = description
-  currentGameMaxNumbers = maxNumber
-  currentGameRange = range
-  currentGameType = type
-  currentGamePrice = price
-}
-
-function renderNumbers(maxNumber, maxRange) {
-  numbers.innerHTML = ''
-  selectedNumber = []
-
-  for (let index = 0; index < maxRange; index++) {
-    let html = `<input type="button" class="gameNumber" value="${
-      index + 1
-    }" id="number" onclick="handleSelectNumber(${index},${maxNumber})">`
-    numbers.innerHTML += html
+    bets = []
+    $bets.innerHTML = ''
+    betTotalValue = 0
+    changeTotalValue()
+    clearSelectedNumbers()
   }
-}
 
-getData()
+  init()
+})()
